e6405cb6aa711a4a80865a1388992b4017d63fd0
John Arboleda <arboledasantillana@gmail.com>
2017-10-11 10:38:53 -0500

    Update README file

--- app/src/main/java/com/johneas/android/movie/view/common/utils/Constants.java
+++ app/src/main/java/com/johneas/android/movie/view/common/utils/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 
     public static final String POSTER_IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     public static final String POSTER_IMAGE_SIZE = "w780";
-    public static final String MOVIE_API_KEY = "66659cb18f0f77b502e277331bb39a4f";
+    public static final String MOVIE_API_KEY = "HERE YOUR API KEY";
 
 }
